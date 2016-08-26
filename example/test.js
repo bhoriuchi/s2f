ffddb6ae3e104e7ceb4859fc6c882fdf5e7ee827
Branden Horiuchi <bhoriuchi@gmail.com>
2016-08-26 14:52:33 -0700

    * updating graphql dependency

--- example/test.js
+++ example/test.js
@@ -6,7 +6,7 @@ let r = rethinkdbdash()
 let backend = new Backend(r, graphql)
 let lib = gql(backend)
 
-// lib.Workflow('mutation Mutation { createStep (workflowId: "d1a7bdc4-032f-46c4-a757-18ebc46a7d78", name: "Step1", type: Basic) { id, name } }')
+// lib.Workflow('mutation Mutation { createStep (workflowId: "d1a7bdc4-032f-46c4-a757-18ebc46a7d78", name: "Step2", type: Basic) { id, name } }')
 // lib.Workflow('mutation Mutation { createWorkflow (name: "TEST") { name, steps { id, type } } }')
 lib.Workflow('{ readWorkflow (id: "d1a7bdc4-032f-46c4-a757-18ebc46a7d78") { id, name, steps { id, type, name } } }')
   .then((res) => {
